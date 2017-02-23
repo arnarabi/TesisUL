b4d3ed683735d22188a4dbccb36ded49fc176bff
Alessandra Fernandez Esquivel <Alessandra Fernandez Esquivel>
2017-02-22 23:18:22 -0500

    asiojdd

--- src/main/java/model/Tesis.java
+++ src/main/java/model/Tesis.java
@@ -18,6 +18,14 @@ public class Tesis {
         this.feedback = feedback;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
   
     public String getTitulo() {
         return titulo;
